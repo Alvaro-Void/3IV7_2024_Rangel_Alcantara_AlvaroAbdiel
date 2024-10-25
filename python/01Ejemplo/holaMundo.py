1dca75a50c0a866ffefeb456bb104a85dd417674
Alvaro <alva.raa316@gmail.com>
2024-10-24 22:27:18 -0600

    Python primeros pasos

---
+++ python/01Ejemplo/holaMundo.py
@@ -0,0 +1,35 @@
+#print("Hola, mundo");
+#Myfinalmessage, change da world
+#definir variable
+
+#x = 17
+
+#print(x)
+
+#ejemplo de if una condicion
+
+#x = 28
+
+#if x < 0:
+    #cuando sea verdad 
+    #print("Es menor que 0")
+#elif x > 0:
+    #print("Es mayor a 0")
+#else: 
+    #print("Es 0")
+
+#bucles
+
+#numero=0
+#print("Tabla del 2")
+
+#while numero<= 10 :
+#    numero +=1
+#    print("Resultado: ", + 2*numero)
+
+#bucle for
+
+numero=[3,7,5,8]
+
+for n in numero :
+    print(n)
---
+++ python/02Entrada/Calculadora.py
@@ -0,0 +1,11 @@
+#vamos a crear una calculadora sencilla
+#Para la entrada de datos es necesario utilizar el metodo
+
+numero1 = float(input("introduce el primer numero: "))
+
+numero2 = float(input("introduce el segundo numero: "))
+
+#toca sumar
+
+suma= numero1+numero2
+print("La suma es de: ", suma)
